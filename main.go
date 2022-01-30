bd72cc5f7db39fe946242def09efb1d3666e27de
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2022-01-31 04:36:59 +0700

    fix 🐛: cors error

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ func main() {
 	defer config.CloseDatabaseConnection(db)
 
 	server := gin.Default()
+	server.Use(CORSMiddleware())
 
 	adviceRoutes := server.Group("api/v1")
 	{
@@ -41,4 +42,25 @@ func main() {
 	}
 
 	server.Run()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "*")
+		/*
+		   c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		   c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		   c.Writer.Header().Set("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers")
+		   c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, OPTIONS, PATCH")
+		*/
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
