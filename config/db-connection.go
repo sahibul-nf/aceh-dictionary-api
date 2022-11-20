45c4f7e0d79f8bb8d4ec0aa0f26378193c8dd04b
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2022-11-21 03:22:27 +0700

    🐞 fix: db connection

--- config/db-connection.go
+++ config/db-connection.go
@@ -5,26 +5,27 @@ import (
 	"aceh-dictionary-api/dictionary"
 	"aceh-dictionary-api/user"
 	"fmt"
+	"os"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func SetupDatabaseConnection() *gorm.DB {
-	// dbUser := os.Getenv("DB_USER")
-	// dbHost := os.Getenv("DB_HOST")
-	// dbName := os.Getenv("DB_NAME")
-	// dbPass := os.Getenv("DB_PASS")
-	// dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	dbPass := os.Getenv("DB_PASS")
+	dbPort := os.Getenv("DB_PORT")
 
 	// Mysql
-	dsn := "root:@tcp(127.0.0.1:3306)/acehnese_dictionary?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		DisableForeignKeyConstraintWhenMigrating: true,
-	})
+	// dsn := "root:@tcp(127.0.0.1:3306)/acehnese_dictionary?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+	// 	DisableForeignKeyConstraintWhenMigrating: true,
+	// })
 	// Postgres
-	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=Asia/Shanghai", dbHost, dbUser, dbPass, dbName, dbPort)
-	// db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=Asia/Shanghai", dbHost, dbUser, dbPass, dbName, dbPort)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to create a connection to database")
