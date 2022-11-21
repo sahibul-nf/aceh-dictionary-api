4c5743cf2bc77eadfe3d942f9aa4f536826eec18
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2022-11-21 22:48:54 +0700

    🎈 perf: update dictionary entity

--- dictionary/entity.go
+++ dictionary/entity.go
@@ -3,10 +3,10 @@ package dictionary
 import "time"
 
 type Dictionary struct {
-	ID        int    `gorm:"column:id;type:int;primaryKey;autoIncrement" json:"id"`
-	Aceh      string `gorm:"column:aceh;type:varchar;size:255" json:"aceh"`
-	Indonesia string `gorm:"column:indonesia;type:varchar;size:255" json:"indonesia"`
-	English   string `gorm:"column:english;type:varchar;size:255" json:"english"`
-	CreatedAt time.Time `gorm:"column:created_at;type:timestamp;default:CURRENT_TIMESTAMP" json:"created_at"`
-	UpdatedAt time.Time `gorm:"column:updated_at;type:timestamp;default:CURRENT_TIMESTAMP" json:"updated_at"`
+	ID        int       `gorm:"column:id;type:int;primaryKey;autoIncrement" json:"id"`
+	Aceh      string    `gorm:"column:aceh;type:varchar;size:255" json:"aceh"`
+	Indonesia string    `gorm:"column:indonesia;type:varchar;size:255" json:"indonesia"`
+	English   string    `gorm:"column:english;type:varchar;size:255" json:"english"`
+	CreatedAt time.Time `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt time.Time `gorm:"column:updated_at" json:"updated_at"`
 }
